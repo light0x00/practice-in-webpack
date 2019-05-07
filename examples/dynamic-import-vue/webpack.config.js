ccdfeb16e2daae3989c765d060de7cc35f1ee77b
light <light0x00@163.com>
2019-05-07 18:02:09 +0800

    dyn imp in vue

--- examples/dynamic-import-vue/webpack.config.js
+++ examples/dynamic-import-vue/webpack.config.js
@@ -3,38 +3,50 @@ const fs = require('fs');
 let CleanWebpackPlugin = require('clean-webpack-plugin')
 let HtmlWebpackPlugin = require('html-webpack-plugin')
 let webpack = require('webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 let config = {
     // mode:"production",
     mode: "development",
-    devServer:{
+    devtool: 'eval-source-map',
+    devServer: {
 
     },
     entry: {
-        entry2: _resolve(__dirname, "src/entry2/main.ts"),
-        entry1: _resolve(__dirname, "src/entry1/main.js"),
+        entry1: _resolve(__dirname, "src/index.ts"),
     }
     ,
     output: {
         path: _resolve(__dirname, 'dist'),
         filename: '[name].bundle.js',
     },
+    resolve: {
+        extensions: ['.vue', '.js', '.ts'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm',
+        }
+    },
     module: {
-        noParse: (content) => /lodash/.test(content),
         rules: [
             {
-                test:/ts/,
-                use:["ts-loader"]
-            }
+                test: /ts/,
+                use: [{ loader: "ts-loader", options: { appendTsSuffixTo: [/\.vue$/] } }]
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+
         ]
     },
     optimization: {
-  
+
     },
     plugins: [
+        new VueLoaderPlugin(),
         new webpack.ProgressPlugin(),
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({ filename: `index.html`, chunks: ["entry1","default","asyncA","common"] }),
+        new HtmlWebpackPlugin({ template: "./index.html" }),
         // new HtmlWebpackPlugin({ filename: `index2.html`, chunks: ["entry2","default","vendors","common"] }),
     ]
 };
