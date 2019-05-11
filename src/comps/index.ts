ffd4af2b22a86e334f59e51e2c9341df0fe3701c
light <light0x00@163.com>
2019-05-11 23:45:52 +0800

    clean

--- src/comps/index.ts
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-//https://webpack.js.org/guides/tree-shaking/   https://webpack.docschina.org/configuration/optimization/#optimization-providedexports
-//https://webpack.docschina.org/plugins/split-chunks-plugin/
-//https://webpack.js.org/api/module-methods/#import
-//https://github.com/ElemeFE/element/blob/master/components.json
-//https://cn.vuejs.org/v2/guide/components-dynamic-async.html#ad
-Vue.component('el-button',() => import('element-ui/packages/button/index.js'))
-Vue.component('el-radio',() => import('element-ui/packages/radio/index.js'))
-Vue.component('el-input',() => import('element-ui/packages/input/index.js'))
