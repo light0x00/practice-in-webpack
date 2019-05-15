21b8ce8d122e33fe702a770a9bfe208c1f6dbf02
light <light0x00@163.com>
2019-05-15 15:52:04 +0800

    resolve element async compiliation error

--- optimization/case4-minify/src/comps/elemeRegister.ts
+++ optimization/case4-minify/src/comps/elemeRegister.ts
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import 'element-ui/lib/theme-chalk/index.css'
-//https://webpack.js.org/guides/tree-shaking/   https://webpack.docschina.org/configuration/optimization/#optimization-providedexports
-//https://webpack.docschina.org/plugins/split-chunks-plugin/
-//https://webpack.js.org/api/module-methods/#import
-//https://github.com/ElemeFE/element/blob/master/components.json
-//https://cn.vuejs.org/v2/guide/components-dynamic-async.html#ad
-Vue.component('el-button',() => import('element-ui/packages/button/index.js'))
-Vue.component('el-radio',() => import('element-ui/packages/radio/index.js'))
-Vue.component('el-input',() => import( 'element-ui/packages/input/index.js'))
+Vue.component('el-button', () => import(/* webpackPrefetch:true */'element-ui/lib/button'))
+Vue.component('el-radio', () => import(/* webpackPrefetch:true */'element-ui/lib/radio'))
+Vue.component('el-input', () => import(/* webpackPrefetch:true */'element-ui/lib/input'))
--- optimization/case6-dll/src/entry1/main.ts
+++ optimization/case6-dll/src/entry1/main.ts
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import 'element-ui/lib/theme-chalk/index.css'
-import '@/comps/index'
+import '@/comps/elemeRegister'
 
 Vue.use(VueRouter)
 
